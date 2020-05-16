8e4a5289db21b2277d9bc3aa1a4a4211a5b01416
Roger <brascoroger@gmail.com>
2020-05-16 22:09:35 +0200

    tab bar and path

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-const Header = (props) => {
+const writeTab = ({key, label, active = false}) => <div className={active ? "tab" : "tab shadow"} id={key}>
+{label}
+<div className={active ? "xBttn" : "xBttn shadow"}>X</div>
+</div>;
 
+const tabBar = (tabs) => <div className="tabBar">
+  {tabs.map(tab => writeTab(tab))}
+</div> 
+
+const path = ({key, label}) => <div className="path">
+      <div className="path_text">{"rogerbras.co >"}</div>
+      <div className="path_text">{key+ " >"}</div>
+      <div className="path_text">{label}</div>
+      </div>;
+
+const Header = (props) => {
+  const tabInfo = [{key: "homepage", label:"info.yml", "active": true}, {key: "aboutme", label:"personal.yml"}]
   return (
     <div className="app__header">
-      <div className="tab">
-        {"info.yml X"}
-      </div>
-      <div className="path">
-        rogerbras.co > homepage > info.yml
-      </div>
+      {tabBar(tabInfo)}
+      {path(tabInfo[0])}
     </div>
   );
 
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -2,24 +2,23 @@ import React from 'react';
 import info from '../../info'
 let i=1;
 
-const line = ({i, key, label, depth=0}) => <div className="line" id={i}>
+const line = ({i, key, label, depth=0, array=false}) => <div className="line" id={i}>
   <div className="n_line">{i}</div>
-  <div className={depth == 0? "key_text": "key_text moved"}>{key}</div>
+  <div className={depth === 0? "key_text": "key_text moved"}>{key}</div>
   {key === "" ? null : <div className="dots">:</div>}
-  <div className={depth == 0? "label_text": "label_text"}>{label}</div>
+  {array ? <div className="dash"> - </div> : null}
+  <div className="label_text">{label}</div>
 </div>;
 
 const printLines = Object.keys(info).map((key) =>
   typeof info[key] === "string" ?  line({"i": i++, "key": key, "label": info[key]}) : 
   <div> 
     {line({"i": i++, "key": key, "label": ""})}
-    {Object.keys(info[key]).map((subkey) => line({"i": i++, "key": info[key] instanceof Array ? "" : subkey, "label": info[key][subkey], "depth":1}))}
+    {Object.keys(info[key]).map((subkey) => line({"i": i++, "key": info[key] instanceof Array ? "" : subkey, "label": info[key][subkey], "depth":1, "array": info[key] instanceof Array }))}
   </div>  
 );
 
 const Homepage = (props) => {
-  console.log(info);
-  Object.keys(info).forEach(key => console.log(info[key]));
 
   return (
     <div className="homepage" >
