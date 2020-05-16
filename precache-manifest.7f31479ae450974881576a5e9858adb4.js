b56fa31161210a8b126e4e08d5596fb4f569c637
Roger <brascoroger@gmail.com>
2020-05-16 17:25:28 +0200

    Updates

---
+++ precache-manifest.7f31479ae450974881576a5e9858adb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16d829a06e0f577323587845a42c4ef",
+    "url": "/cv_page/index.html"
+  },
+  {
+    "revision": "9496da93cfe3a986ae07",
+    "url": "/cv_page/static/css/main.9c38931b.chunk.css"
+  },
+  {
+    "revision": "7b5f5c787469974456a8",
+    "url": "/cv_page/static/js/2.2a000cbe.chunk.js"
+  },
+  {
+    "revision": "9496da93cfe3a986ae07",
+    "url": "/cv_page/static/js/main.41b29152.chunk.js"
+  },
+  {
+    "revision": "2163c3c64943365479b1",
+    "url": "/cv_page/static/js/runtime-main.58ef258b.js"
+  }
+]);
