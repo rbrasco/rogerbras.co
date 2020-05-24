9034a2d66fd04cc3630a98246f41accf5d09fd5a
Roger <brascoroger@gmail.com>
2020-05-24 13:43:23 +0200

    Updates

---
+++ precache-manifest.903bc37126051d65b7c36337f2483437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0f09c6f4702737ba66f725a42f4d1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be96673ec7f718923529",
+    "url": "/static/css/main.a808dd60.chunk.css"
+  },
+  {
+    "revision": "a2300f39232001944293",
+    "url": "/static/js/2.0c32c870.chunk.js"
+  },
+  {
+    "revision": "be96673ec7f718923529",
+    "url": "/static/js/main.903f6948.chunk.js"
+  },
+  {
+    "revision": "f670876c961a15c1cd96",
+    "url": "/static/js/runtime-main.0fffdbd2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f58dd6c98689b0f0226266152d442a2.js"
+  "/precache-manifest.903bc37126051d65b7c36337f2483437.js"
 );
 
 self.addEventListener('message', (event) => {
