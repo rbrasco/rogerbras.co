73c721b19de35871b01ec390c247b74fe8003dfa
Roger <brascoroger@gmail.com>
2020-05-27 20:01:38 +0200

    dynamic size

--- src/components/wButtons/index.js
+++ src/components/wButtons/index.js
@@ -4,12 +4,16 @@ import MaximizeIcon from '@material-ui/icons/Maximize';
 import CloseIcon from '@material-ui/icons/Close';
 
 const WButtons = () =>  <div className="wBttns">
-    <div className="wSpace"></div>
+    <div className="trans">
+        <MinimizeIcon className="wIcons" fontSize="small"></MinimizeIcon>
+        <MaximizeIcon className="wIcons" fontSize="small"></MaximizeIcon>
+        <CloseIcon className="wIcons red" fontSize="small"></CloseIcon>
+    </div>
     <div className="wTitle">Personal Page Project - CODE EDITOR</div>
     <div className="wIconsWrapper">
-    <MinimizeIcon className="wIcons" fontSize="small"></MinimizeIcon>
-    <MaximizeIcon className="wIcons" fontSize="small"></MaximizeIcon>
-    <CloseIcon className="wIcons red" fontSize="small"></CloseIcon>
+        <MinimizeIcon className="wIcons" fontSize="small"></MinimizeIcon>
+        <MaximizeIcon className="wIcons" fontSize="small"></MaximizeIcon>
+        <CloseIcon className="wIcons red" fontSize="small"></CloseIcon>
     </div>
 </div>;
 
