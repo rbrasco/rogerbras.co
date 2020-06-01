6f6bd667dd12504d88ba982212dc62af75c9be18
Roger <brascoroger@gmail.com>
2020-06-01 15:33:25 +0200

    404 file

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
   render () {
 
     return (
-      <BrowserRouter>
+      <BrowserRouter >
         <Window dict={dict} isMobile={isMobile}/>
         {isMobile ? null : <StarField />}
       </BrowserRouter>
