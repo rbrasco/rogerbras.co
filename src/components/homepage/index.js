dd67d1e9c5d8c71dcd9e5126d60401f07357d91f
Roger <brascoroger@gmail.com>
2020-06-15 12:38:05 +0200

    New links

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import AsciiCanvas from '../asciiCanvas';
 import { withRouter } from 'react-router-dom';
+import pdf from '../../files/cv_Roger_Brasco.pdf';
+const vscode = 'https://code.visualstudio.com/';
+const dracula = 'https://draculatheme.com/';
+const reactjs = 'https://reactjs.org/';
+
+const linkTag = (key, text, style, title = '') => <a href={key} target='_blank' rel="noopener noreferrer" className={style} title={title}>{text}</a>;
 
 const Homepage = () =>
   <div className="default">
@@ -10,11 +16,12 @@ const Homepage = () =>
       <p className="dText">Welcome to my original idea for a <span className="dYellow">CV page</span></p>
       <p className="dText">It pretends to be a window of a code editor</p>
       <p className="dText">Where every <span className="dPurple">.yml</span> file displays information in a nice way</p>
-      <p className="dText">Inspired by <span className="dRed">VS CODE</span> and <span className="dRed">{"Dracula's Theme"}</span> colors</p>
-      <p className="dText">Complitly done with <span className="dPurple">Reactjs</span>, full code at <span className="dRed">Github</span></p>
+      <p className="dText">
+        Inspired by {linkTag(vscode, 'VS CODE', 'dRed')} and {linkTag(dracula, "Dracula's Theme", 'dRed')} colors</p>
+      <p className="dText">Complitly done with {linkTag(reactjs, 'Reactjs', 'dPurple')}, full code at <span className="dRed">Github</span></p>
       <p className="dText">Click on a tab to <span className="dYellow">start</span></p>
-      <p className="dText"><span className="dBlue">PDF version</span></p>
-      <p className="dText">JUNE - 2020</p>
+      <p className="dText">{linkTag(pdf, 'PDF version', 'dBlue', 'Download CV')}</p>
+      <p className="dText"><span className="dPink">JUNE - 2020</span></p>
     </div>
   </div>;
 
