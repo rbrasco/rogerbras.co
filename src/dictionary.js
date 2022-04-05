12036dd4526fdf1d5916b1abe7d1184d0acb5573
Roger Brasco <brascoroger@gmail.com>
2022-04-05 18:40:11 +0200

    Review

--- src/dictionary.js
+++ src/dictionary.js
@@ -24,12 +24,69 @@ const info = {
   ],
 };
 
+var now = new Date();
+var bDay = new Date(2021, 12, 1);
+var elapsedT = now - bDay; // in ms
+console.log(elapsedT);
+
+function getDaysInMonth(month, year) {
+  if (typeof year == "undefined") year = 1999; // any non-leap-year works as default
+  var currmon = new Date(year, month),
+    nextmon = new Date(year, month + 1);
+  return Math.floor(
+    (nextmon.getTime() - currmon.getTime()) / (24 * 3600 * 1000)
+  );
+}
+function getDateTimeSince(target) {
+  // target should be a Date object
+  var now = new Date(),
+    yd,
+    md,
+    dd,
+    hd,
+    nd,
+    sd,
+    out = [];
+
+  yd = now.getFullYear() - target.getFullYear();
+  md = now.getMonth() - target.getMonth();
+  dd = now.getDate() - target.getDate();
+  hd = now.getHours() - target.getHours();
+  nd = now.getMinutes() - target.getMinutes();
+  sd = now.getSeconds() - target.getSeconds();
+
+  if (md < 0) {
+    yd--;
+    md += 12;
+  }
+  if (dd < 0) {
+    md--;
+    dd += getDaysInMonth(now.getMonth() - 1, now.getFullYear());
+  }
+  if (hd < 0) {
+    dd--;
+    hd += 24;
+  }
+  if (nd < 0) {
+    hd--;
+    nd += 60;
+  }
+  if (sd < 0) {
+    nd--;
+    sd += 60;
+  }
+
+  if (yd > 0) out.push(yd + " yr" + (yd === 1 ? "" : "s"));
+  if (md > 0) out.push(md + " mo" + (md === 1 ? "" : "s"));
+  return out.join(" ");
+}
+console.log();
 const experience = {
   Title: "Work experience by importance",
   Current: {
     Company: "Shalion",
     Position: "Product Manager Junior",
-    Duration: "Dec-2021 - Present",
+    Duration: `Dec 2021 - Present (${getDateTimeSince(new Date(2021, 11, 1))})`,
     Job: [
       "Define and manage the development of new features of sold products",
       "Carry out training to the overseas operations team",
@@ -38,7 +95,7 @@ const experience = {
   Top: {
     Company: "Shalion",
     Position: "Research & Development intern",
-    Duration: "1 year",
+    Duration: " Feb 2021 - Dec 2021 (11 mos)",
     Job: [
       "Develop automation scripts for the operations team",
       "Start products that are sold to clients",
@@ -49,7 +106,7 @@ const experience = {
   Mid: {
     Company: "Scytl",
     Position: "Software developer intern",
-    Duration: "1 year",
+    Duration: "Jun 2019 - May 2020 (1 yr)",
     Job: [
       "Took part in the team of managers on the services Bitbucket, Jira, Jenkins, and Nexus",
       "Continuous integration and process automation scripts",
@@ -84,7 +141,7 @@ const education = {
   Languages: {
     Catalan: "Native",
     Spanish: "Native",
-    Eglish: "Advanced",
+    English: "Advanced",
   },
 };
 
